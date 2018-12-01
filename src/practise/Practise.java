bd627910c139bbc22834535ebac07b0c9c1fe33f
tawhidurislam <sesire_isalm@yahoo.com>
2018-12-01 17:40:17 -0500

    practise

---
+++ src/practise/Practise.java
@@ -0,0 +1,12 @@
+package practise;
+
+public class Practise {
+
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 20;
+        int result = x + y;
+        System.out.println("result = " + result);
+
+    }
+}
